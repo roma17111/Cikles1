3aef906cb2c4d9b079537af8fcff651d4108a2ce
Roman <romanze1706@gmail.com>
2022-11-10 10:13:10 +0300

    задание 1.3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i <=10; i++) {
-            System.out.println("Число " + i);}
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Число " + i);
+        }
 
-        for (int b =10; b >=0; b-- ) {
-            System.out.println("Число " + b);
+        for (int b = 10; b >= 0; b--) {
+            System.out.println("Число " + b);}
+
+            for ( int c = 2; c < 17; c = c+2) {
+                System.out.println("Число " + c);
+            }
 
         }
     }
-}

40120f0bfa541bab66f8c07be839eeba5cb8940c
Roman <romanze1706@gmail.com>
2022-11-10 10:27:40 +0300

    задание 2.1

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,21 @@ public class Main {
         }
 
         for (int b = 10; b >= 0; b--) {
-            System.out.println("Число " + b);}
+            System.out.println("Число " + b);
+        }
 
-            for ( int c = 2; c < 17; c = c+2) {
-                System.out.println("Число " + c);
-            }
-            for (int d = 10; d >= -10; d --) {
-                System.out.println("Число " + d);
-            }
+        for (int c = 2; c < 17; c = c + 2) {
+            System.out.println("Число " + c);
+        }
+        for (int d = 10; d >= -10; d--) {
+            System.out.println("Число " + d);
+        }
 
+        for (int y = 1904; y <= 2096; y += 4) {
+            System.out.println(y + " год является высокосным");
 
         }
+
+
     }
+}

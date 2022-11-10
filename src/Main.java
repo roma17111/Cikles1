0901ff9284aa22fdc9f2f1b486d5c7ac61911d1c
Roman <romanze1706@gmail.com>
2022-11-10 09:57:43 +0300

    задание 1.2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
 
         for (int i = 0; i <=10; i++) {
             System.out.println("Число " + i);}
+
+        for (int b =10; b >=0; b-- ) {
+            System.out.println("Число " + b);
+
+        }
     }
 }

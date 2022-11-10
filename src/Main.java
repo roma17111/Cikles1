2a4e2ee2c848874a29c4a6662eddc2c515c58327
Roman <romanze1706@gmail.com>
2022-11-10 11:33:37 +0300

    задание 3.2

--- src/Main.java
+++ src/Main.java
@@ -30,8 +30,10 @@ public class Main {
         int deposit = 29000;
         int total = 0;
         for (int m = 1; m <= 12; m++) {
+            total = total + total /100;
             total = total + deposit;
             System.out.println("Месяц " + m + " сумма накоплений равна " + total + " рублей.");
+
         }
 
 
